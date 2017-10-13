e469a17c52cadd1789425986e33c75c8324955e2
efri-yang <948061564@qq.com>
2017-10-13 17:28:09 +0800

    c

--- src/MyPhpProject/MoKuaiRongHeXiangMu/modules/imageGD/demo/test.php
+++ src/MyPhpProject/MoKuaiRongHeXiangMu/modules/imageGD/demo/test.php
@@ -1,4 +1,5 @@
 <?php
+	include("./imageftcenter.func.php");
 	$src="../images/01.jpg";
 
 	$imgInfo=getimagesize($src);
@@ -14,6 +15,8 @@
 
 
     $font="./msyh.ttc";
+    $fontSize=20;
+    $angle=0;
 
     $white = imagecolorallocate($image, 255, 255, 255);
 	$grey = imagecolorallocate($image, 128, 128, 128);
@@ -24,12 +27,23 @@
 	$greyO= imagecolorallocatealpha($image, 128, 128, 128,75);
 	$blackO = imagecolorallocatealpha($image, 0, 0, 0,75);
 
-	$stampW=200;
+	$text1="杨艺辉520";
+	$text2="杨艺辉5201314";
+
+	$stampW=400;
 	$stampH=100;
 	$stampBg=imagecreatetruecolor($stampW, $stampH);
 
 	imagefilledrectangle($stampBg, 0, 0, imagesx($stampBg),imagesy($stampBg),$blackO);
-	
+
+	list($x)=ImageFTCenter($stampBg,$fontSize,$angle,$font,$text1);
+	imagettftext($stampBg,20,0,$x, 30,$white,$font,$text1);
+	list($x)=ImageFTCenter($stampBg,$fontSize,$angle,$font,$text2);
+	imagettftext($stampBg,20,0, $x, 60,$white,$font,$text2);
+
+	imagecopymerge($image,$stampBg,imagesx($image)-$stampW, imagesy($image)-$stampH, 0, 0, $stampW, $stampH, 50);
+
+
 
 	// $text1="杨艺辉520";
 	// $text1="杨艺辉5201314";
@@ -48,7 +62,7 @@
 
 
 
-	header("content-type:image/jpeg");
+	header("content-type:$imageMime");
 	imagepng($image);
 	imagedestroy($image);
 ?>
