8118a3779efd0eae91b595b44723a8e4132dea7c
efri-yang <948061564@qq.com>
2017-10-18 17:43:52 +0800

    c

--- src/MyPhpProject/MoKuaiRongHeXiangMu/template/nav.php
+++ src/MyPhpProject/MoKuaiRongHeXiangMu/template/nav.php
@@ -2,7 +2,10 @@
 	<ul class="cnav">
 		<li><a href="<?php echo APP_ROOT_URL."/index.php"; ?>">首页</a></li>
 		<li><a href="<?php echo APP_ROOT_URL."/modules/fileManager/index.php"; ?>">文件管理</a></li>
-		<li><a href="#">图片水印</a></li>
+		<li><a href="<?php echo APP_ROOT_URL."/modules/imageGD/index.php"; ?>">图片水印</a></li>
 		<li><a href="<?php echo APP_ROOT_URL."/modules/imageEJZ/index.php"; ?>">图片二进制存储</a></li>
+
+
+		<li><a href="<?php echo APP_ROOT_URL."/modules/lunTan/index.php"; ?>">论坛管理</a></li>
 	</ul>
 </div>
