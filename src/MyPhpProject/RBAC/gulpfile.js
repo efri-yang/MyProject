9049aa0097fe18be472dc8c6470137fd0609a3b4
efri-yang <948061564@qq.com>
2017-12-04 10:24:41 +0800

    c

--- src/MyPhpProject/RBAC/gulpfile.js
+++ src/MyPhpProject/RBAC/gulpfile.js
@@ -2,21 +2,58 @@
 
 const gulp = require("gulp");
 
-const htmls = require('./FontEndGulp/htmls');
-const styles = require('./FontEndGulp/styles');
-const scripts = require('./FontEndGulp/scripts');
-const images = require('./FontEndGulp/images');
-const others = require('./FontEndGulp/others');
-const watchs = require('./FontEndGulp/watch');
-const servers = require('./FontEndGulp/server');
-const cleans = require('./FontEndGulp/clean');
+// const htmls = require('./FontEndGulp/htmls');
+// const styles = require('./FontEndGulp/styles');
+// const scripts = require('./FontEndGulp/scripts');
+// const images = require('./FontEndGulp/images');
+// const others = require('./FontEndGulp/others');
+// const watchs = require('./FontEndGulp/watch');
+// const servers = require('./FontEndGulp/server');
+// const cleans = require('./FontEndGulp/clean');
 
 
 
 
 
 
-gulp.task('dev', gulp.series(cleans.DevClean, gulp.parallel(gulp.series(scripts.DevScripts,styles.DevStyles),htmls.DevHtmls,images.DevImages,others.DevOthers,watchs.DevWatchs,servers.DevServer)));
+// gulp.task('dev', gulp.series(cleans.DevClean, gulp.parallel(gulp.series(scripts.DevScripts,styles.DevStyles),htmls.DevHtmls,images.DevImages,others.DevOthers,watchs.DevWatchs,servers.DevServer)));
+// 
+// 
+
+var rev = require('gulp-rev');
+
+gulp.task('css', function () {
+    return gulp.src('FontEndSrc/**/*.css')
+        .pipe(rev())
+        .pipe(gulp.dest('FontEndDev'))
+        .pipe( rev.manifest() )
+        .pipe( gulp.dest( 'rev/css' ) );
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('FontEndSrc/**/*.js')
+        .pipe(rev())
+        .pipe(gulp.dest('FontEndDev'))
+        .pipe( rev.manifest() )
+        .pipe( gulp.dest( 'rev/js' ) );
+});
+
+
+
+var revCollector = require('gulp-rev-collector');
+
+
+gulp.task('rev', function () {
+    return gulp.src(['rev/**/*.json', 'FontEndDev/**/*.html'])
+        .pipe( revCollector({
+            replaceReved: true,
+            dirReplacements: {
+                'css': '/dist/css'
+            }
+        }) )
+      
+        .pipe( gulp.dest('FontEndDev') );
+});
 
 
 
