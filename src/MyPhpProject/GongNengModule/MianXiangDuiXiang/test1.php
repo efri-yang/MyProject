a8e958f3d477d320264bef4f1955e0a32e47a75c
efri-yang <948061564@qq.com>
2018-01-23 09:27:35 +0800

    c

---
+++ src/MyPhpProject/GongNengModule/MianXiangDuiXiang/test1.php
@@ -0,0 +1,33 @@
+<?php
+	
+	// function get_file($filename){
+	// 	return file_get_contents($filename);
+	// }
+
+	// $function="get_file";
+	// $args=array("https://mmbiz.qpic.cn/mmbiz_gif/uxWj6c30TTuxoAmVhySQuDKqyj5NHYFA5vVRFQics8DGxGqXbhz8lOECdTQM9j8Uuicmj84lqQpZ2NIsfpicfluXw/?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1");
+
+
+	// echo call_user_func_array($function,$args);
+
+	
+	$food="pizza";
+	$drink="beer";
+
+	function party(){
+		global $food,$drink;
+		unset($food);
+		unset($GLOBALS['drink']);
+	}
+
+	print "$food:$drink"."<br/>";
+	party();
+	print "$food:$drink"."<br/>";
+	//pizza:beer
+	//pizza:
+
+
+	
+
+	
+?>
