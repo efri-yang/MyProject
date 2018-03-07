53428937a3daf313e83296e77120206356a2ceeb
efri-yang <948061564@qq.com>
2018-03-07 08:53:18 +0800

    c

--- src/MyPhpCms/TPVoting/application/admin/controller/User.php
+++ src/MyPhpCms/TPVoting/application/admin/controller/User.php
@@ -4,10 +4,9 @@ namespace app\admin\controller;
 use app\admin\model\AuthUser;
 use think\Config;
 use think\Controller;
+use think\File;
 use think\Request;
 use think\Url;
-use think\File;
-
 
 class User extends Base {
     public function index() {
@@ -26,16 +25,28 @@ class User extends Base {
         $file = request()->file('file');
         if ($file) {
 
-            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads'. DS .'admin'. DS .'avatar');
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'admin' . DS . 'avatar');
             if ($info) {
-                $authUser=new AuthUser();
-                $filePath=$info->getSaveName();
-                $authUser->save(["avatar"=>$filePath],["id"=>1]);
-                return json_encode(array("filePath"=>$info->getSaveName()));
+                $authUser = new AuthUser();
+                $filePath = $info->getSaveName();
+                $authUser->save(["avatar" => $filePath], ["id" => 1]);
+                return json_encode(array("filePath" => $info->getSaveName()));
             } else {
                 return 0;
-               
+
             }
+        }
+    }
+    public function updateProfile() {
+        $request = Request::instance();
+        $params = $request->param();
+
+        $authUser = new AuthUser();
+        $res = $authUser->allowField(true)->save($params, ["id" => 1]);
+        if ($res !== false) {
+            $this->success('成功！');
+        } else {
+            $this->error('失败！');
         }
     }
 }
