bc82c3f047e6549b6cb48d4fa69498b59c636df0
efri-yang <948061564@qq.com>
2018-04-12 17:54:42 +0800

    c

--- src/MyPhpCms/TPAdmin/application/admin/controller/AdminUser.php
+++ src/MyPhpCms/TPAdmin/application/admin/controller/AdminUser.php
@@ -2,6 +2,8 @@
 namespace app\admin\controller;
 
 use app\admin\model\AuthUser;
+use app\admin\model\AuthGroup;
+
 
 class AdminUser extends Base {
     public function index() {
@@ -9,11 +11,29 @@ class AdminUser extends Base {
         $authUser = new AuthUser();
         $authUser->where('id', '<>', 1);
         $lists = $authUser->select()->toArray();
+
         $this->assign("list", $lists);
         return $this->fetch();
     }
     public function profile() {
         return $this->fetch();
+    }
+
+    public function  add(){
+        //要去auth_group 查询所有的角色
+        $groupList=AuthGroup::all()->toArray();
+        if($this->request->isPost()){
+            $param=$this->request->param();
+            //
+
+        }else{
+            $this->assign("groupList",$groupList);
+            return $this->fetch();
+        }
+       
+        
+        
+        
     }
 
 }
