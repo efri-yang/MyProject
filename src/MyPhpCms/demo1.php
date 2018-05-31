fd429cf8e32b9e28f4f87bd4c2f26013efd1f575
efri-yang <948061564@qq.com>
2018-05-31 15:41:55 +0800

    c

--- src/MyPhpCms/demo1.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$rootPath = $_SERVER['DOCUMENT_ROOT'];
-	echo $rootPath;
-	$file="G:/xampp/htdocs/MyProject/src/MyPhpProject/NeiRongGuanLiXiTong/staitc/images/header_bg.png";
-	$dirname = dirname($file);
-	echo $dirname;
-?>
