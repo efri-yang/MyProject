a47ea1657f56210910f763b80d6eea938c305097
efri-yang <948061564@qq.com>
2019-01-02 15:20:29 +0800

    2019 jihua

--- src/TestDel/str1.php
+++ src/TestDel/str1.php
@@ -1,23 +1,6 @@
 <?php
-
-// 11
-function lookandsay($s) {
-    $r = '';
-    $m = $s[0]; //第一次的时候$s不是字符窜$s[0]就是空
-    $n = 1;
-    for ($i = 1, $j = strlen($s); $i < $j; $i++) {
-        if ($s[$i] == $m) {
-            $n++;
-        } else {
-            $r .= $n . $m;
-            $m = $s[$i];
-            $n = 1;
-        }
-    }
-    return $r . $n . $m;
-
-}
-
-print substr("watch out for thatxree", -5);
+$autoload=10;
+$a=$autoload ?: 'cccc';
+echo $a;
 
 ?>
