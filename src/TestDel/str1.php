6636541797e72810afe747cfbea49f5826206f78
efri-yang <948061564@qq.com>
2019-12-31 10:00:58 +0800

    YYH

--- src/TestDel/str1.php
+++ src/TestDel/str1.php
@@ -1,31 +1,5 @@
 <?php
 
-class Father {
-
-    public function getNewFather() {
-        return new self();
-    }
-
-    public function getNewCaller() {
-        return new static();
-    }
-
-}
-class Sun1 extends Father {
-
-}
-
-class Sun2 extends Father {
-
-}
-$sun1 = new Sun1();
-$sun2 = new Sun2();
-print get_class($sun1->getNewFather());
-echo "<br/>";
-print get_class($sun1->getNewCaller());
-echo "<br/>";
-print get_class($sun2->getNewFather());
-echo "<br/>";
-print get_class($sun2->getNewCaller());
+echo md5("+");
 ?>
   
